b5cdabbd85f2141f9efc8d96fa536f67e659f66b
Minhyuk Song <98685266+BeMatthewsong@users.noreply.github.com>
2023-12-19 11:23:44 +0900

    Create App.jsx

---
+++ usePrevious/App.jsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { usePrevious } from "./usePrevious";
+
+function getRandomColor() {
+  const colors = ["green", "blue", "purple", "red", "pink"];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+export default function App() {
+  const [color, setColor] = React.useState(getRandomColor());
+  const previousColor = usePrevious(color); // 이전 색상 담아두기
+
+  const handleClick = () => {
+    function getNewColor() {
+      const newColor = getRandomColor();
+      if (color === newColor) {
+        getNewColor();
+      } else {
+        setColor(newColor);
+      }
+    }
+    getNewColor();
+  };
+
+  return (
+    <section>
+      <h1>usePrevious</h1>
+      <button className="link" onClick={handleClick}>
+        Next
+      </button>
+      <article>
+        <figure>
+          <p style={{ background: `var(--${previousColor})` }} />
+          <figcaption>Previous: {previousColor}</figcaption>
+        </figure>
+        <figure>
+          <p style={{ background: `var(--${color})` }} />
+          <figcaption>Current: {color}</figcaption>
+        </figure>
+      </article>
+    </section>
+  );
