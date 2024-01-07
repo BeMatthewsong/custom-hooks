ebfd036757be8360b5f105ed472620be29335a0b
Minhyuk Song <98685266+BeMatthewsong@users.noreply.github.com>
2024-01-07 20:58:48 +0900

    Create useResize.jsx

---
+++ useResize/useResize.jsx
@@ -0,0 +1,25 @@
+// 화면의 너비와 높이를 받아오는 함수입니다. 
+// 
+import { useEffect, useState } from 'react';
+
+export const useResize = () => {
+  const [windowSize, setWindowSize] = useState<SizeType>({
+    width: undefined,
+    height: undefined,
+  });
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+};
